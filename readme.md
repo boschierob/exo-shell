bdd0e75479c4c404cf881102fc7af9126b7f545b
boschierob <bosc8088@gmail.com>
2020-01-21 17:56:00 +0100

    ajout du readme

---
+++ readme.md
@@ -0,0 +1,9 @@
+Ici les fichiers des exercices sur le shell du programme CodingAccelerator
+Site des exercices: https://www.learnshell.org/
+
+Comprenant les solutions aux exercices sur :
+- les variables
+- les array comparaisons
+- les strings operators
+- les loops
+- les functions
--- variables.sh
+++ variables.sh
@@ -8,17 +8,18 @@ BIRTHDAY=`date -d "$BIRTHDATE" +%A`
 # Testing code - do not change it
 
 if [ "$BIRTHDATE" == "Jan 1, 2000" ] ; then
-    echo "BIRTHDATE is correct, it is $BIRTHDATE"
+    echo "BIRTHDATE est correct, c'est $BIRTHDATE"
 else
-    echo "BIRTHDATE is incorrect - please retry"
+    echo "BIRTHDATE est incorrect - réessayez"
 fi
 if [ $Presents == 10 ] ; then
-    echo "I have received $Presents presents"
+    echo "J'ai recu $Presents cadeaux"
 else
     echo "Presents is incorrect - please retry"
 fi
 if [ "$BIRTHDAY" == "Saturday" ] ; then
-    echo "I was born on a $BIRTHDAY"
+    echo "Je suis né un $BIRTHDAY"
 else
     echo "BIRTHDAY is incorrect - please retry"
 fi
+
